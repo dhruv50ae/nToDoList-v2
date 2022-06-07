194a4e003d8bb2c00b219c94e334a90ac7a3b728
dhruv50ae <linkdp2012@gmail.com>
2022-06-07 21:45:36 +0100

    Use EJS to render days

--- app.js
+++ app.js
@@ -2,15 +2,39 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   let today = new Date();
   let currentDay = today.getDay();
-  if (currentDay === 6 || currentDay === 0) {
-    res.write("<h1>It's the weekend, time to rest</h1>");
-  } else {
-    res.sendFile(__dirname + "/index.html");
+  let day = "";
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+    default:
+      console.log("There is no 8th day, unless... 👀");
+      break;
   }
+  res.render("list", { kindOfDay: day });
 });
 
 app.listen(3000, () => {
