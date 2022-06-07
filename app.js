405348e16b158aa2a103daa15f91317f489e1017
dhruv50ae <linkdp2012@gmail.com>
2022-06-07 21:28:04 +0100

    Create template

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello");
+  let today = new Date();
+  let currentDay = today.getDay();
+  if (currentDay === 6 || currentDay === 0) {
+    res.write("<h1>It's the weekend, time to rest</h1>");
+  } else {
+    res.sendFile(__dirname + "/index.html");
+  }
 });
 
 app.listen(3000, () => {
