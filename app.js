d80e7b4ef086de01e76e9f5c58d49183a2e0cb37
dhruv50ae <linkdp2012@gmail.com>
2022-06-10 19:28:11 +0100

    Create ToDo interface

--- app.js
+++ app.js
@@ -2,39 +2,28 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+let items = ["Buy food", "Cook food", "Eat food"];
 app.set("view engine", "ejs");
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("./public"));
 app.get("/", (req, res) => {
   let today = new Date();
-  let currentDay = today.getDay();
-  let day = "";
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-      console.log("There is no 8th day, unless... 👀");
-      break;
-  }
-  res.render("list", { kindOfDay: day });
+
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+  let day = today.toLocaleDateString("en-US", options);
+
+  res.render("list", { kindOfDay: day, newListItem: items });
+});
+
+app.post("/", (req, res) => {
+  let item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
 });
 
 app.listen(3000, () => {
